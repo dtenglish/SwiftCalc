6e6b4f7735a53666e95b6fde2b04159bf801fefe
Daniel Taylor English <danieltaylorenglish@gmail.com>
2022-06-06 10:55:36 -0700

    Initial Commit

---
+++ SwiftCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCalc
+//
+//  Created by Daniel Taylor English on 6/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftCalc/SwiftCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCalcApp.swift
+//  SwiftCalc
+//
+//  Created by Daniel Taylor English on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

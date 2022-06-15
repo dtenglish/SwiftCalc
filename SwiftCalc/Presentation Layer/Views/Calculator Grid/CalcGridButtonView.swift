7c7164a541723f5a5e4813afc3880c6c062fb677
Daniel Taylor English <danieltaylorenglish@gmail.com>
2022-06-15 11:46:27 -0700

    added simple animation on button press

--- SwiftCalc/Presentation Layer/Views/Calculator Grid/CalcGridButtonView.swift
+++ SwiftCalc/Presentation Layer/Views/Calculator Grid/CalcGridButtonView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CalcGridButtonView: View {
     //MARK: - PROPERTIES
+    @State private var isTapped = false
+    
     let button: CalcGridButton
     let width: CGFloat
     let height: CGFloat
@@ -18,16 +20,25 @@ struct CalcGridButtonView: View {
     var body: some View {
         ZStack {
             Capsule()
-                .foregroundColor(button.backgroundColor)
+                .foregroundColor(button.backgroundColor.opacity(isTapped ? 0 : 1))
                 .frame(width: width, height: height)
             Text(button.label.rawValue)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .foregroundColor(button.textColor)
         }
+        .scaleEffect(isTapped ? 1.1 : 1)
         .onTapGesture {
+            withAnimation(.easeInOut) {
+                isTapped = true
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                     isTapped = false
+                }
+            }
+            
             action()
-        }
+         }
     }
 }
 
